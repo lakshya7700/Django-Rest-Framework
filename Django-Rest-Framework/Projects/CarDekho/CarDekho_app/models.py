b5434354189daea775b47b502b95f7764df615a9
Lakshya <lakshya.sks@gmail.com>
2025-01-17 16:41:35 +0530

    Created car field model and set up superuser

--- Django-Rest-Framework/Projects/CarDekho/CarDekho_app/models.py
+++ Django-Rest-Framework/Projects/CarDekho/CarDekho_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CarList(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.CharField(max_length=1000)
+    active=models.BooleanField(default=False)

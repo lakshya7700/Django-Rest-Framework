078b791433f1ae1ed1ee7d624277c15d29ff9130
Lakshya <lakshya.sks@gmail.com>
2025-01-17 20:16:02 +0530

    Modified url.py file and created views

--- Django-Rest-Framework/Projects/CarDekho/CarDekho_app/urls.py
+++ Django-Rest-Framework/Projects/CarDekho/CarDekho_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('list',views.car_list_view, name='car_list')
+    path('list',views.car_list_view, name='car_list'),
 ]
--- Django-Rest-Framework/Projects/CarDekho/CarDekho_app/views.py
+++ Django-Rest-Framework/Projects/CarDekho/CarDekho_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import CarList
+from django.http import JsonResponse
 
 # Create your views here.
+
+def car_list_view(request):
+    cars=CarList.objects.all()
+    # converting into dictionary
+    data={
+        'cars':list(cars.values()),
+    }
+    return JsonResponse(data)
